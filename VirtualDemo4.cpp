1dd5a348e2e9e3e35165ae57cd2bb8fe02c22528
Siddhant-balwadkar <siddhantvbalwadkar@gmail.com>
2025-10-04 14:03:28 +0530

    Add files via upload

---
+++ VirtualDemo4.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+    public:
+        int i,j;
+        void fun()
+        {
+            cout<<"Inside Base fun\n";
+        }
+        void gun()
+        {
+            cout<<"Inside Base gun\n";
+        }
+        virtual void sun()
+        {
+            cout<<"Inside Base sun\n";
+        }
+        virtual void bun()
+        {
+            cout<<"Inside Base bun\n";
+        }
+
+        
+};
+
+class Derived:public Base
+{
+    public:
+        int x;
+        void gun()
+        {
+            cout<<"Inside Derived gun\n";
+        }
+        void sun()
+        {
+            cout<<"Inside Derived sun\n";
+        }
+        void run()
+        {
+            cout<<"Inside Derived run\n";
+        }
+        void mun()
+        {
+            cout<<"Inside Derived mun\n";
+        }
+};
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";
+    cout<<sizeof(Derived)<<"\n";
+
+    Base *bp = new Derived();
+
+
+    /*bp->fun();
+    bp->gun();
+    bp->sun();
+    //bp->run();
+    //bp->mun();
+    bp->bun();
+    */
+
+    return 0;
+}
